bc2bbdfe7d18c5567550201ee993a41d7b4673c7
Alex Clemmer <clemmer.alexander@gmail.com>
2015-08-02 13:48:35 -0700

    Tweaks definitions in windows.hpp to fit real compilation problems.
    Essentially, there are some definitions that need to be there, and some
    that are not quite right.
    
    This commit adds and removes the definitions required to get the ip
    tests to pass. We also add some nice comments.

--- 3rdparty/libprocess/3rdparty/stout/include/stout/windows.hpp
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/windows.hpp
@@ -11,34 +11,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef __STOUT_WINDOWS_PREPROCESSOR_HPP__
-#define __STOUT_WINDOWS_PREPROCESSOR_HPP__
+#ifndef __STOUT_WINDOWS_HPP__
+#define __STOUT_WINDOWS_HPP__
 
-// Provides aliases to Windows-specific nuances.
+// DEFINITIONS AND CONSTANTS USED FOR WINDOWS COMPAT.
+//
+// Putting all the "compat" definitions here makes it possible for files
+// throughout the codebase to remain relatively free of all the #if's we'd need
+// to make them work.
+//
+// Roughly, the things that should go in this file are definitions and
+// declarations that you would not mind:
+//   * being in global scope
+//   * gloabally available throughout both the Stout codebase, and any code
+//     that links to it (such as Mesos).
 
 // Normally defined in unistd.h.
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
 
-// Alias for method in stdio.h.
+// TODO(hausdorff): audit whether we really want `write` to be a macro. On the
+// one hand it does allow files like abort.hpp to be one file, but on the other
+// hand, anyone who includes abort will now have this macro rolling around in
+// global scope. cf. -DNOMINMAX for an example of the pain this causes IRL.
 #define write(fd, buf, count) _write(fd, buf, count)
-
-// Aliases for 'inet_pton' and 'inet_ntop'.
-#define inet_pton(af, cp, buf) InetPton(af, cp, buf)
-#define inet_ntop(af, cp, buf, len) InetNtop(af, cp, buf, len)
 
 // TODO(aclemmer): Not defined on Windows.  This value is temporary.
 #define MAXHOSTNAMELEN 64
 
 // Macros associated with ::access, usually defined in unistd.h.
+// TODO(hausdorff): see comment above about auditing function macros.
 #define access(path, how) _access(path, how)
 #define R_OK 0x4
 #define W_OK 0x2
 #define X_OK 0x0 // No such permission on Windows
 #define F_OK 0x0
 
-// Aliases for file access modes (defined in fcntl.h).
+// Aliases for file access modes.
+#include <fcntl.h>
 #define O_RDONLY _O_RDONLY
 #define O_WRONLY _O_WRONLY
 #define O_RDWR _O_RDWR
@@ -48,14 +59,17 @@
 // TODO(josephw): No equivalent for O_NONBLOCK or O_SYNC
 
 // Alias for mkstemp (requires io.h).
+// TODO(hausdorff): see comment above about auditing function macros.
 #define mkstemp(path) _mktemp_s(path, strlen(path) + 1)
 
 // Alias for realpath.
 #define PATH_MAX MAX_PATH
+// TODO(hausdorff): see comment above about auditing function macros.
 #define realpath(path, resolved) _fullpath(resolved, path, PATH_MAX)
 
 // Alias for mkdir (requires direct.h).
+// TODO(hausdorff): see comment above about auditing function macros.
 #define mkdir(path, mode) _mkdir(path)
 
 
-#endif // __STOUT_WINDOWS_PREPROCESSOR_HPP__
+#endif // __STOUT_WINDOWS_HPP__
