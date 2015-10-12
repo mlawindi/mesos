0903d78e4a9e4aad7e35250ae03565243dd36229
Alex Clemmer <clemmer.alexander@gmail.com>
2015-10-11 20:05:37 -0700

    Windows: Added `stout/os/chsize.hpp`.
    Review: https://reviews.apache.org/r/39217

---
+++ 3rdparty/libprocess/3rdparty/stout/include/stout/os/windows/chsize.hpp
@@ -0,0 +1,40 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __STOUT_OS_WINDOWS_CHSIZE_HPP__
+#define __STOUT_OS_WINDOWS_CHSIZE_HPP__
+
+#include <io.h>
+
+#include <stout/try.hpp>
+
+
+namespace os {
+
+// Identical in functionality to POSIX standard `ftruncate`, except that on
+// Windows the file descriptor that we pass as an argument must belong to an
+// open file.
+inline Try<Nothing> chsize(int fd, off_t length)
+{
+  if (::_chsize(fd, length) != 0) {
+    return ErrnoError(
+      "Failed to truncate file at file descriptor '" + std::to_string(fd) +
+      "' to " + std::to_string(length) + " bytes.");
+  }
+
+  return Nothing();
+}
+
+} // namespace os {
+
+#endif // __STOUT_OS_WINDOWS_CHSIZE_HPP__
